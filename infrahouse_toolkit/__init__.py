fa233a891f2fe533f38f5cff3a6404746f126e7d
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-02-18 11:38:07 -0800

    Bump version: 0.2.0 → 1.0.0

--- infrahouse_toolkit/__init__.py
+++ infrahouse_toolkit/__init__.py
@@ -2,13 +2,13 @@
 Top-level package for InfraHouse Toolkit.
 
 :Author: Oleksandr Kuzminskyi
-:Version: 0.2.0
+:Version: 1.0.0
 :Email: aleks@infrahouse.com
 """
 
 __author__ = """Oleksandr Kuzminskyi"""
 __email__ = "aleks@infrahouse.com"
-__version__ = "0.2.0"
+__version__ = "1.0.0"
 
 
 DEFAULT_OPEN_ENCODING = "utf8"
--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/infrahouse/infrahouse-toolkit",
-    version="0.2.0",
+    version="1.0.0",
     zip_safe=False,
 )
