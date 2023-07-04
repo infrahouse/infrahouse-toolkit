b653738843d94bd3a64a0aee59212c10f1ec7b53
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-07-04 15:40:34 -0700

    Add --existing-actions
    Why is it needed?
    
    Let's say you already have a policy but you found that it's missing some
    permissions. The terraform trace will include actions only withing the
    latest run. However you'd like to keep existing permissions and just
    want to extent the list.
    
    The `--existing-actions` option allows to specify a file with known
    permissions. Its format is
    ```
    [
          "ec2:AllocateAddress",
          "ec2:AssociateRouteTable",
    ...
          "ec2:AssociateRouteTable",
    ]
    ```
    i.e. a JSON list.
    
    Then the `min-permissions` will produce the updated list of necessary
    privileges and show the difference.

--- infrahouse_toolkit/cli/ih_plan/cmd_min_permissions/__init__.py
+++ infrahouse_toolkit/cli/ih_plan/cmd_min_permissions/__init__.py
@@ -6,6 +6,7 @@
     See ``ih-plan min-permissions --help`` for more details.
 """
 import json
+from copy import deepcopy
 from json import JSONDecodeError
 
 import click
@@ -14,8 +15,9 @@ from infrahouse_toolkit import DEFAULT_OPEN_ENCODING
 
 
 @click.command(name="min-permissions")
+@click.option("--existing-actions", help="A file with permissions.", default=None)
 @click.argument("trace_file")
-def cmd_min_permissions(trace_file):
+def cmd_min_permissions(existing_actions, trace_file):
     """
     Parse Terraform trace file and produce an action list from the trace.
 
@@ -36,6 +38,16 @@ def cmd_min_permissions(trace_file):
 
     """
     actions = []
+    if existing_actions:
+        with open(existing_actions, encoding=DEFAULT_OPEN_ENCODING) as f_desc:
+            actions = json.loads(f_desc.read())
+
+    old_actions = deepcopy(actions)
+    initial_count = len(old_actions)
+
+    print(f"## Existing {initial_count} actions:")
+    print(json.dumps(sorted(old_actions), indent=4))
+
     with open(trace_file, encoding=DEFAULT_OPEN_ENCODING) as f_decs:
         for line in f_decs.readlines():
             try:
@@ -45,4 +57,10 @@ def cmd_min_permissions(trace_file):
             except JSONDecodeError:
                 pass
 
+    new_actions = list(set(actions))
+    final_count = len(new_actions)
+    print(f"## {final_count - initial_count} new action(s):")
+    print(json.dumps([x for x in new_actions if x not in old_actions], indent=4))
+
+    print(f"## Old and new actions together, {final_count} in total:")
     print(json.dumps(sorted(list(set(actions))), indent=4))
