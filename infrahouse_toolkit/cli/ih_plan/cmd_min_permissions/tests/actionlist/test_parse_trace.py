7f17069b5001073557b15571ba873f6517d4441b
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-10-15 13:43:19 -0700

    ih-plan min-permissions supports aws provider > 5.11
    The AWS terraform provider changed logging somewhere after the version
    5.11.
    
    It used to log AWS API calls like
    ```
    {"aws.operation": "GetBucketCors","aws.service": "S3"}
    
    ```
    and after the 5.11 the equivalent log message looks like
    ```
    {""rpc.method": "GetBucketCors","rpc.service": "S3"}
    ```
    
    The tool looks for both from now on.

--- infrahouse_toolkit/cli/ih_plan/cmd_min_permissions/tests/actionlist/test_parse_trace.py
+++ infrahouse_toolkit/cli/ih_plan/cmd_min_permissions/tests/actionlist/test_parse_trace.py
@@ -1,5 +1,7 @@
 from textwrap import dedent
 
+import pytest
+
 from infrahouse_toolkit.cli.ih_plan.cmd_min_permissions import ActionList
 
 
@@ -21,29 +23,45 @@ def test_parse_trace(tmpdir):
     ]
 
 
-def test_parse_trace_permissions_map(tmpdir):
+@pytest.mark.parametrize(
+    "trace_content, expected_permissions",
+    [
+        (
+            dedent(
+                """
+                {"aws.operation": "HeadBucket","aws.service": "S3"}
+                {"aws.operation": "GetBucketAccelerateConfiguration","aws.service": "S3"}
+                {"aws.operation": "GetBucketEncryption","aws.service": "S3"}
+                {"aws.operation": "GetBucketCors","aws.service": "S3"}
+                {"aws.operation": "GetBucketLifecycleConfiguration","aws.service": "S3"}
+                {"aws.operation": "GetBucketReplication","aws.service": "S3"}
+                {"aws.operation": "GetObjectLockConfiguration","aws.service": "S3"}
+                """
+            ),
+            [
+                "s3:GetAccelerateConfiguration",
+                "s3:GetBucketCORS",
+                "s3:GetBucketObjectLockConfiguration",
+                "s3:GetEncryptionConfiguration",
+                "s3:GetLifecycleConfiguration",
+                "s3:GetReplicationConfiguration",
+                "s3:ListBucket",
+            ],
+        ),
+        (
+            dedent(
+                """
+                {"rpc.method": "CreateBucket", "rpc.service": "S3"}
+                {"rpc.method": "CreateTable", "rpc.service": "DynamoDB"}
+                """
+            ),
+            ["s3:CreateBucket", "dynamodb:CreateTable"],
+        ),
+    ],
+)
+def test_parse_trace_permissions_map(tmpdir, trace_content, expected_permissions):
     tracefile = tmpdir.join("trace")
-    tracefile.write(
-        dedent(
-            """
-            {"aws.operation": "HeadBucket","aws.service": "S3"}
-            {"aws.operation": "GetBucketAccelerateConfiguration","aws.service": "S3"}
-            {"aws.operation": "GetBucketEncryption","aws.service": "S3"}
-            {"aws.operation": "GetBucketCors","aws.service": "S3"}
-            {"aws.operation": "GetBucketLifecycleConfiguration","aws.service": "S3"}
-            {"aws.operation": "GetBucketReplication","aws.service": "S3"}
-            {"aws.operation": "GetObjectLockConfiguration","aws.service": "S3"}
-            """
-        )
-    )
+    tracefile.write(trace_content)
     actions = ActionList()
     actions.parse_trace(str(tracefile))
-    assert actions.actions == [
-        "s3:GetAccelerateConfiguration",
-        "s3:GetBucketCORS",
-        "s3:GetBucketObjectLockConfiguration",
-        "s3:GetEncryptionConfiguration",
-        "s3:GetLifecycleConfiguration",
-        "s3:GetReplicationConfiguration",
-        "s3:ListBucket",
-    ]
+    assert sorted(actions.actions) == sorted(expected_permissions)
