207e8c167e8f913a96d836338cd57401cfe968de
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-12-30 11:41:54 -0800

    Bump version: 2.9.5 → 2.9.6

--- infrahouse_toolkit/__init__.py
+++ infrahouse_toolkit/__init__.py
@@ -2,13 +2,13 @@
 Top-level package for InfraHouse Toolkit.
 
 :Author: Oleksandr Kuzminskyi
-:Version: 2.9.5
+:Version: 2.9.6
 :Email: aleks@infrahouse.com
 """
 
 __author__ = """Oleksandr Kuzminskyi"""
 __email__ = "aleks@infrahouse.com"
-__version__ = "2.9.5"
+__version__ = "2.9.6"
 
 from logging import getLogger
 
--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/infrahouse/infrahouse-toolkit",
-    version="2.9.5",
+    version="2.9.6",
     zip_safe=False,
 )
