131a68464b574e1ceaefc64656304b3f8d128efc
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-03-30 15:25:36 -0700

    Add new subcommand ih-github runner is-registered
    * `ih-github runner is-registered` to check if a runner is already
    registered.
    * Add ability to read a GitHub token from AWS secrets manager. IMO it's
    safer than environment variables.

--- infrahouse_toolkit/cli/ih_github/cmd_runner/__init__.py
+++ infrahouse_toolkit/cli/ih_github/cmd_runner/__init__.py
@@ -7,11 +7,16 @@
 """
 import logging
 
+import boto3
 import click
 
 from infrahouse_toolkit.cli.ih_github.cmd_runner.cmd_deregister import cmd_deregister
+from infrahouse_toolkit.cli.ih_github.cmd_runner.cmd_is_registered import (
+    cmd_is_registered,
+)
 from infrahouse_toolkit.cli.ih_github.cmd_runner.cmd_list import cmd_list
 from infrahouse_toolkit.cli.ih_github.cmd_runner.cmd_register import cmd_register
+from infrahouse_toolkit.cli.ih_secrets.cmd_get import get_secret
 
 LOG = logging.getLogger()
 
@@ -19,7 +24,8 @@ LOG = logging.getLogger()
 @click.group(
     name="runner",
 )
-@click.option("--github-token", help="Personal access token for GitHub.", envvar="GITHUB_TOKEN")
+@click.option("--github-token", help="Personal access token for GitHub.", envvar="GITHUB_TOKEN", show_default=True)
+@click.option("--github-token-secret", help="Read GitHub token from AWS secret.")
 @click.option(
     "--org",
     help="GitHub organization",
@@ -34,9 +40,14 @@ def cmd_runner(ctx, *args, **kwargs):
     LOG.debug("args = %s", args)
     LOG.debug("kwargs = %s", kwargs)
     LOG.debug(ctx.args)
-    ctx.obj = {"github_token": kwargs["github_token"], "org": kwargs["org"]}
+    if kwargs["github_token_secret"]:
+        github_token = get_secret(boto3.client("secretsmanager"), kwargs["github_token_secret"])
+    else:
+        github_token = kwargs["github_token"]
+
+    ctx.obj = {"github_token": github_token, "org": kwargs["org"]}
 
 
-for cmd in [cmd_list, cmd_register, cmd_deregister]:
+for cmd in [cmd_list, cmd_register, cmd_deregister, cmd_is_registered]:
     # noinspection PyTypeChecker
     cmd_runner.add_command(cmd)
