4bb80044d667da9a06a77fa996dbb1662b7918d5
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2024-07-20 18:04:28 -0700

    fix shell lint error

--- omnibus-infrahouse-toolkit/build-arm64.sh
+++ omnibus-infrahouse-toolkit/build-arm64.sh
@@ -10,7 +10,7 @@ do
     docker run --rm \
         --privileged \
         $(ih-aws --aws-profile AWSAdministratorAccess-493370826424 credentials --docker) \
-        -v $PWD:/infrahouse-toolkit \
+        -v "$PWD":/infrahouse-toolkit \
         -w /infrahouse-toolkit \
         ubuntu:noble bash /infrahouse-toolkit/omnibus-infrahouse-toolkit/upload-arm64.sh $os
 done
