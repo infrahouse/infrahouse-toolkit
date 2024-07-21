4fefd218e2274faabec4b8ab3fe3ab44f05503a4
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-07-20 18:08:36 -0700

    Add scripts for building infrahouse-toolkit on arm64 (#109)
    * Add scripts for building infrahouse-toolkit on arm64
    
    Well, I couldn't install docker on a macos-14 GitHub Actions runner.
    But I still need infrahouse-toolkit packages on arm64.
    As a workaround, I created a `build-arm64.sh` build script. It's supposed to run on a MacOS laptop with arm64 chip.
    The script builds the package for Ubuntu focal, jammy, and noble and uploads the package to https://release-${OS_VERSION}.infrahouse.com/
    
    * fix shell lint error
    
    ---------
    
    Co-authored-by: Oleksandr Kuzminskyi <aleks@infrahouse.com>

---
+++ omnibus-infrahouse-toolkit/build-arm64.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eux
+
+for os in "noble" "jammy" "focal"
+do
+    export OS_VERSION=$os
+    make clean
+    make package
+    docker run --rm \
+        --privileged \
+        $(ih-aws --aws-profile AWSAdministratorAccess-493370826424 credentials --docker) \
+        -v "$PWD":/infrahouse-toolkit \
+        -w /infrahouse-toolkit \
+        ubuntu:noble bash /infrahouse-toolkit/omnibus-infrahouse-toolkit/upload-arm64.sh $os
+done
