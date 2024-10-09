ddcae8386d31af4668338b2b2be38ca5ba506121
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-10-09 15:35:03 -0700

    Add a timestamp suffix to github repo copies (#123)
    * Add suffix to a repository copy
    
    The S3 service doesn't work well with directories. aws s3 sync,
    for instance, doesn't upload empty directories to a bucket. As result,
    the repo mirror is invalid as it doesn't contain tags and some other
    directories.
    
    * Bump version: 2.33.0 → 2.34.0

--- setup.py
+++ setup.py
@@ -71,6 +71,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/infrahouse/infrahouse-toolkit",
-    version="2.33.0",
+    version="2.34.0",
     zip_safe=False,
 )
