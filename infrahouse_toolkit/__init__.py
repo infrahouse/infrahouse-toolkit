c2e7361c4677d32e75e7f0101e6c238c439c37f6
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-10-22 11:18:37 -0700

    Set 'The cluster status is %s - not green' to INFO (#133)
    * Set 'The cluster status is %s - not green' to INFO
    
    * Bump version: 2.39.2 → 2.39.3

--- infrahouse_toolkit/__init__.py
+++ infrahouse_toolkit/__init__.py
@@ -2,13 +2,13 @@
 Top-level package for InfraHouse Toolkit.
 
 :Author: Oleksandr Kuzminskyi
-:Version: 2.39.2
+:Version: 2.39.3
 :Email: aleks@infrahouse.com
 """
 
 __author__ = """Oleksandr Kuzminskyi"""
 __email__ = "aleks@infrahouse.com"
-__version__ = "2.39.2"
+__version__ = "2.39.3"
 
 DEFAULT_OPEN_ENCODING = "utf8"
 DEFAULT_ENCODING = DEFAULT_OPEN_ENCODING
