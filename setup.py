acc7c155c3fc77bb82ecb357db299f46d1fe3d7b
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-12-20 15:20:50 -0800

    Install InfraHouse repository (#143)
    * Install InfraHouse repository
    
    This is needed to reprepro 5.4
    
    * Install the repo with sudo
    
    * Escape a dollar sign
    
    * Install the repo in CD
    
    * Bump version: 2.45.0 → 2.45.1

--- setup.py
+++ setup.py
@@ -71,6 +71,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/infrahouse/infrahouse-toolkit",
-    version="2.45.0",
+    version="2.45.1",
     zip_safe=False,
 )
