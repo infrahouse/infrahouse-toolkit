824c2bb995384244738f947fee2e5e8b26d08552
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-03-24 09:37:35 -0700

    Add the command implementation

--- infrahouse_toolkit/cli/ih_github/cmd_scan/__init__.py
+++ infrahouse_toolkit/cli/ih_github/cmd_scan/__init__.py
@@ -6,11 +6,16 @@
     See ``ih-github scan --help`` for more details.
 """
 
+import json
 import logging
+import sys
+from datetime import datetime, timedelta
+from subprocess import PIPE, Popen
 
 import click
 
 from infrahouse_toolkit.cli.utils import check_dependencies
+from infrahouse_toolkit.terraform.githubpr import GitHubPR
 
 LOG = logging.getLogger()
 DEPENDENCIES = ["osv-scanner"]
@@ -21,8 +26,12 @@ DEPENDENCIES = ["osv-scanner"]
     context_settings={"ignore_unknown_options": True, "allow_extra_args": True},
 )
 @click.option("--github-token", help="Personal access token for GitHub.", envvar="GITHUB_TOKEN")
-@click.option("--repo", help="GitHub repository name in short format e.g. infrahouse-toolkit.")
+@click.option("--repo", help="GitHub repository name in a long format e.g. infrahouse/infrahouse-toolkit.")
 @click.option("--pull-request", help="GitHub pull request number.", type=click.INT)
+@click.option("--sla-critical", help="SLA in days on when critical vulnerabilities should be fixed.", default=15)
+@click.option("--sla-high", help="SLA in days on when high vulnerabilities should be fixed.", default=30)
+@click.option("--sla-medium", help="SLA in days on when medium vulnerabilities should be fixed.", default=45)
+@click.option("--sla-low", help="SLA in days on when medium vulnerabilities should be fixed.", default=90)
 @click.pass_context
 def cmd_scan(ctx, *args, **kwargs):
     """
@@ -37,39 +46,84 @@ def cmd_scan(ctx, *args, **kwargs):
     LOG.debug("cmd_args = %s", cmd_args)
     check_dependencies(DEPENDENCIES)
     # pull_request = GitHubPR(kwargs["repo"], kwargs["pull_request_number"], github_token=kwargs["github_token"])
-    # try:
-    #     with Popen(cmd, stderr=PIPE, stdout=PIPE) as proc:
-    #         LOG.info("Launched command: %s", " ".join(cmd))
-    #
-    #         start_time = time.time()
-    #         stop_time = start_time + kwargs["run_timeout"]
-    #         return_code = None
-    #         wait_time = 1
-    #         while time.time() < stop_time:
-    #             return_code = proc.poll()
-    #             if return_code is None:
-    #                 if time.time() - start_time > 1:
-    #                     LOG.info("%s is still running. Waiting %d seconds more.", cmd[0], stop_time - time.time())
-    #                 wait_time = wait_time * 2 if wait_time < 10 else 10
-    #                 time.sleep(wait_time)
-    #             else:
-    #                 break
-    #
-    #         cout, cerr = proc.communicate()
-    #
-    #         comment = f"Command `{' '.join(cmd)}` exited with code {return_code}.\n"
-    #         comment += f"**Standard output**:\n```\n{cout.decode() or 'No output'}\n```\n"
-    #         comment += f"**Standard error**:\n```\n{cerr.decode() or 'No output'}\n```\n"
-    #
-    #         sys.stdout.write(cout.decode())
-    #         sys.stderr.write(cerr.decode())
-    #         sys.exit(proc.returncode)
-    #
-    # except FileNotFoundError as err:
-    #     LOG.exception(err)
-    #     comment = f"Command `{' '.join(cmd)}` failed to start.\n"
-    #     comment += f"Stack trace:\n```\n{traceback.format_exc()}\n```\n"
-    #     sys.exit(1)
-    #
-    # finally:
-    #     pull_request.publish_comment(comment)
+    cmd = ["osv-scanner", "scan", "--format", "markdown", "--recursive", "--verbosity", "warn", "./"]
+    cmd.extend(cmd_args)
+    sla_map = {
+        "CRITICAL": kwargs["sla_critical"],
+        "HIGH": kwargs["sla_high"],
+        "MODERATE": kwargs["sla_medium"],
+        "MEDIUM": kwargs["sla_medium"],
+        "LOW": kwargs["sla_low"],
+    }
+    comment = None
+    try:
+        with Popen(cmd, stderr=PIPE, stdout=PIPE) as proc:
+            LOG.info("Launched command: %s", " ".join(cmd))
+            vuln_table, cerr = proc.communicate()
+            return_code = proc.returncode
+            if cerr:
+                LOG.error(cerr)
+            if return_code != 0:
+                comment = "# Vulnerabilities report\n"
+                comment += vuln_table.decode()
+                for vuln in _get_vulnerability_details(cmd_args):
+                    future_date = datetime.now() + timedelta(days=sla_map.get(vuln["severity"], 30))
+                    comment += f"""
+## {vuln["name"]}=={vuln["version"]}
+The package `{vuln["name"]}` (version {vuln["version"]}) has a {vuln["severity"]}-severity vulnerability.
+
+If you can upgrade to non-vulnerable version, please do.
+If you can't upgrade now, you can append this entry to `osv-scanner.toml`
+
+```
+[[IgnoredVulns]]
+id = "{vuln["id"]}"
+ignoreUntil = {future_date.strftime("%Y-%m-%d")} # Optional exception expiry date
+reason = "Detailed explanation why the vulnerability is ignored and how it is planned to be fixed."
+```
+"""
+            sys.stdout.write(vuln_table.decode())
+
+    finally:
+        if comment:
+            print(comment)
+            if kwargs["repo"] and kwargs["pull_request"]:
+                pull_request = GitHubPR(kwargs["repo"], kwargs["pull_request"], github_token=kwargs["github_token"])
+                pull_request.publish_comment(comment)
+
+    sys.exit(return_code)
+
+
+def _get_vulnerability_details(extra_args):
+    """
+    returns a JSON
+    [
+        {
+            name: "pymysql"
+            version: "1.2.3"
+            id: "GHSA-7wqh-767x-r66v"
+            severity: "MODERATE"
+        }
+    ]
+    """
+    cmd = ["osv-scanner", "scan", "--format", "json", "--recursive", "--verbosity", "warn", "./"]
+    cmd.extend(extra_args)
+    return_value = []
+    with Popen(cmd, stderr=PIPE, stdout=PIPE) as proc:
+        LOG.debug("Launched command: %s", " ".join(cmd))
+        cout, cerr = proc.communicate()
+        if cerr:
+            LOG.error(cerr)
+        response = json.loads(cout)
+        for result in response["results"]:
+            for package_item in result["packages"]:
+                for vuln in package_item["vulnerabilities"]:
+                    return_value.append(
+                        {
+                            "name": package_item["package"]["name"],
+                            "version": package_item["package"]["version"],
+                            "id": vuln["id"],
+                            "severity": vuln["database_specific"]["severity"],
+                        }
+                    )
+        return return_value
