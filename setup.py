202fc2f24a38542cab91908033aff9015d16eb7e
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-04-02 11:36:50 -0700

    Bump version: 2.50.1 → 2.51.0

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/infrahouse/infrahouse-toolkit",
-    version="2.50.1",
+    version="2.51.0",
     zip_safe=False,
 )
