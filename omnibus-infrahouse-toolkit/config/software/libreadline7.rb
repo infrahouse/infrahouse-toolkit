6cb7567286ee164cf21bbe0ff8fba0d07ae08e53
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-05-12 15:17:26 -0700

    Ubuntu deleted readline_7.0

--- omnibus-infrahouse-toolkit/config/software/libreadline7.rb
+++ omnibus-infrahouse-toolkit/config/software/libreadline7.rb
@@ -11,7 +11,7 @@ default_version "7.0"
 
 version("7.0") { source sha256: "750d437185286f40a369e1e4f4764eda932b9459b5ec9a731628393dd3d32334" }
 
-source url: "http://archive.ubuntu.com/ubuntu/pool/main/r/readline/readline_#{version}.orig.tar.gz"
+source url: "ftp://ftp.gnu.org/pub/gnu/readline/readline-#{version}.tar.gz"
 
 license "GNU"
 license_file "README"
